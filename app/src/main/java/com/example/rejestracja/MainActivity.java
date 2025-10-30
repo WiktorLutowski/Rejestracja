13d8d3a9d37f3869ee27d62eaed902a80cdc2643
wlutowski <lutowski.wikto@szkola.elektrykplcok.ud.pl>
2025-10-30 12:18:31 +0100

    Kod

--- app/src/main/java/com/example/rejestracja/MainActivity.java
+++ app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rejestracja;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView resultText;
+    private EditText mail;
+    private EditText password;
+    private EditText repeatPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +26,30 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            resultText = findViewById(R.id.resultView);
+            mail = findViewById(R.id.mail);
+            password = findViewById(R.id.password);
+            repeatPassword = findViewById(R.id.repeatPassword);
+            findViewById(R.id.buttonId).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!mail.getText().toString().contains("@"))
+                    {
+                        resultText.setText("Niepoprawny mail");
+                        return;
+                    }
+
+                    if(password.getText() != repeatPassword.getText())
+                    {
+                        resultText.setText("Hasła nie są takie same");
+                        return;
+                    }
+
+                    resultText.setText("witaj" + mail.getText());
+                }
+            });
             return insets;
         });
     }
+
 }
